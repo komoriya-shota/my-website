64d18f492c2b0e9c69fa23f68932e0b597e4a6a1
komoriya-shota <125643244+komoriya-shota@users.noreply.github.com>
2024-06-24 02:14:16 +0900

    更新

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -23,21 +23,21 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none";  
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // 5秒ごとにスライドを変更
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 document.getElementById('commentForm').addEventListener('submit', function(event) {
